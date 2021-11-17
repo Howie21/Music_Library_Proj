1fed2c7eb06a6a2ac2bb8c84a7cf9e7df8fd153b
Howie21 <bryce9643@gmail.com>
2021-11-17 14:03:59 -0500

    Added table functionality for all songs caught by the API

--- music_library/src/component/DisplayAll/DisplayAll.jsx
+++ music_library/src/component/DisplayAll/DisplayAll.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
-
+import "./DisplayAll.css"
 
 class DisplayAll extends Component {
     constructor(props) {
@@ -18,16 +18,41 @@ class DisplayAll extends Component {
         let songs = await axios.get("http://127.0.0.1:8000/music/");
         console.log(songs);
         this.setState({ 
-            listOfSongs: songs.data.data    
+            listOfSongs: songs.data
         });
-        console.log(this.listOfSongs)
+        console.log(this.state.listOfSongs)
     }
 
 
     render() { 
         return ( 
-            <div style={{padding:"24px"}}>
-                <h3>All Songs</h3>
+            <div className="allSongs">
+                <table className="tablestyle">
+                    <thead className="tableRows">
+                        <th>Title</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Genre</th>
+                        <th>Release Date</th>
+                        <th>Song ID</th>
+                    </thead>
+                    <tbody>
+                        {this.state.listOfSongs.map(song => {
+                            return(
+                                <tr>
+                                    <td>{song.title}</td>
+                                    <td>{song.artist}</td>
+                                    <td>{song.album}</td>
+                                    <td>{song.genre}</td>
+                                    <td>{song.release_date}</td>
+                                    <td>{song.id}</td>
+                                    
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                
             </div>
          );
     }
