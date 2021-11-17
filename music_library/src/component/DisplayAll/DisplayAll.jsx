886ea780e7fb50b8e54b48b7a8606f9b4b709c70
Howie21 <bryce9643@gmail.com>
2021-11-17 16:35:23 -0500

    Changed functionality of Component from Class to Functional

--- music_library/src/component/DisplayAll/DisplayAll.jsx
+++ music_library/src/component/DisplayAll/DisplayAll.jsx
@@ -1,43 +1,83 @@
-import React, { Component } from 'react';
-import axios from "axios";
+import React from 'react';
 import "./DisplayAll.css"
 
-class DisplayAll extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            listOfSongs: []
-         }
-    }
+// class DisplayAll extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { 
+//             listOfSongs: []
+//          }
+//     }
 
-    componentDidMount() {
-        this.getAllSongs();
-    }
+//     componentDidMount() {
+//         this.getAllSongs();
+//     }
 
-    async getAllSongs() {
-        let songs = await axios.get("http://127.0.0.1:8000/music/");
-        console.log(songs);
-        this.setState({ 
-            listOfSongs: songs.data
-        });
-        console.log(this.state.listOfSongs)
-    }
+//     async getAllSongs() {
+//         let songs = await axios.get("http://127.0.0.1:8000/music/");
+//         console.log(songs);
+//         this.setState({ 
+//             listOfSongs: songs.data
+//         });
+//         console.log(this.state.listOfSongs)
+//     }
 
 
-    render() { 
-        return ( 
-            <div className="allSongs">
-                <table className="tablestyle">
-                    <thead className="tableRows">
-                        <th>Title</th>
-                        <th>Artist</th>
-                        <th>Album</th>
-                        <th>Genre</th>
-                        <th>Release Date</th>
-                        <th>Song ID</th>
-                    </thead>
-                    <tbody>
-                        {this.state.listOfSongs.map(song => {
+//     render() { 
+//         return ( 
+//             <div className="allSongs">
+//                 <h2>All Songs available!</h2>
+//                 <table className="tablestyle">
+//                     <thead className="tableRows">
+//                         <th>Title</th>
+//                         <th>Artist</th>
+//                         <th>Album</th>
+//                         <th>Genre</th>
+//                         <th>Release Date</th>
+//                         <th>Song ID</th>
+//                         <th>Delete</th>
+//                     </thead>
+//                     <tbody>
+//                         {this.state.listOfSongs.map(song => {
+//                             return(
+//                                 <tr>
+//                                     <td>{song.title}</td>
+//                                     <td>{song.artist}</td>
+//                                     <td>{song.album}</td>
+//                                     <td>{song.genre}</td>
+//                                     <td>{song.release_date}</td>
+//                                     <td>{song.id}</td>
+//                                     <td><button onClick={this.props.deleteSongMethod(song)}>Delete</button></td>
+                                    
+//                                 </tr>
+//                             )
+//                         })}
+//                     </tbody>
+//                 </table>
+                
+//             </div>
+//          );
+//     }
+// }
+ 
+// export default DisplayAll;
+
+function DisplayAll(props) {
+    return ( 
+        <div className="allSongs">
+                 <h2>All Songs available!</h2>
+                 <table className="tablestyle">
+                     <thead className="tableRows">
+                         <th>Title</th>
+                         <th>Artist</th>
+                         <th>Album</th>
+                         <th>Genre</th>
+                         <th>Release Date</th>
+                         <th>Song ID</th>
+                         <th>Delete</th>
+                     </thead>
+                     <tbody>
+                         {props.listOfSongs.map(song => {
                             return(
                                 <tr>
                                     <td>{song.title}</td>
@@ -46,6 +86,7 @@ class DisplayAll extends Component {
                                     <td>{song.genre}</td>
                                     <td>{song.release_date}</td>
                                     <td>{song.id}</td>
+                                    <td><button onClick={() => props.deleteSongMethod(song)}>Delete</button></td>
                                     
                                 </tr>
                             )
@@ -54,8 +95,7 @@ class DisplayAll extends Component {
                 </table>
                 
             </div>
-         );
-    }
+     );
 }
- 
+
 export default DisplayAll;
