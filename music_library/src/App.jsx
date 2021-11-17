1abb1e90471b73c68e7a2f50d3168d5ea24c2c55
Howie21 <bryce9643@gmail.com>
2021-11-17 16:34:39 -0500

    Added Methods to App to handle the get and pull request of the API and forwarded the information to the DisplayComponent

--- music_library/src/App.jsx
+++ music_library/src/App.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import Header from './component/Header/Header';
 import DisplayAll from './component/DisplayAll/DisplayAll';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-
+      listOfSongs: []
      }
   }
+
+  componentDidMount() {
+    this.getAllSongs();
+  }
+
+  async getAllSongs() {
+    let songs = await axios.get("http://127.0.0.1:8000/music/");
+    console.log(songs);
+    this.setState({ 
+      listOfSongs: songs.data
+    });
+    console.log(this.state.listOfSongs)
+  }
+
+  deleteSong = async (song) => {
+    let songId = song.id
+    await axios.delete(`http://127.0.0.1:8000/music/${songId}/`);
+    this.getAllSongs();
+  }
+
+
   render() { 
     return ( 
-      <div>
+      <div className="container-fluid">
         <Header />
-        <DisplayAll />
+        <DisplayAll deleteSongMethod ={this.deleteSong} listOfSongs ={this.state.listOfSongs} />
       </div>
      );
   }
